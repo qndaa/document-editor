514239a536f65f42f1c6f788fb08441954c6e55d
LukaKrickovic <krickovicluka@gmail.com>
2022-03-20 19:21:33 +0100

    feature: added model notebook

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,83 @@
-import React, {useCallback, useState} from 'react';
-import 'draft-js/dist/Draft.css';
-import './App.css'
+import React, { useCallback, useState } from "react";
+import "draft-js/dist/Draft.css";
+import "./App.css";
 import ReactQuill from "react-quill";
-import {debounce} from "lodash";
+import { debounce } from "lodash";
+import axios from "axios";
 
 const example_words = [
-    'at',
-    'start',
-    'end',
-    'justice',
-    'failed',
-    'guilty',
-    'loyalty',
-    'other'
+  "at",
+  "start",
+  "end",
+  "justice",
+  "failed",
+  "guilty",
+  "loyalty",
+  "other",
 ];
 
 function App() {
+  const [input, setInput] = useState("");
+  const [words, setWords] = useState([
+    example_words[0],
+    example_words[1],
+    example_words[3],
+  ]);
 
-    const [input, setInput] = useState('')
-    const [words, setWords] = useState([example_words[0], example_words[1], example_words[3]]);
+  const sendOnAPI = (text) => {
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/predict/`, {
+        text: input,
+        type: "Agency agreement",
+      })
+      .then((res) => console.table(res));
+  };
 
-    const sendOnAPI = (text) => {
-        console.log(text);
-    }
+  const api = useCallback(debounce(sendOnAPI, 1000), []);
 
+  const onChangeInput = (event) => {
+    api(event);
+  };
 
-    const api = useCallback(
-        debounce(sendOnAPI, 1000)
-        , [])
+  const appendWord = (word) => {
+    console.log(word);
+    setInput(input.concat(" ", word));
+  };
 
-    const onChangeInput = (event) => {
-
-        api(event);
-    }
-
-    const appendWord = (word) => {
-        console.log(word)
-        setInput(input.concat(' ', word))
-    }
-
-    const renderWords = () => {
-        return words.map((word) => {
-            return (
-                <div className={`col-4`} key={word} >
-                    <div className="alert alert-primary" role="alert" onClick={() => appendWord(word)}>
-                        {word}
-                    </div>
-                </div>
-            )
-        })
-    }
-
-    return (
-        <div className={`container`}>
-            <div className={`d-flex justify-content-center`}>
-                <h1 className={`text-dark mt-5`}>Document editor</h1>
-            </div>
-            <div className={`d-flex justify-content-center mb-5 mt-4`}>
-                <label className={`p-2 fw-bold`}>Document type:</label>
-                <select className={`form-select w-50`}>
-                    <option>X</option>
-                    <option>Y</option>
-
-                </select>
-            </div>
-            <ReactQuill
-                value={input}
-                onChange={onChangeInput}
-                bounds={`.app`}
-            />
-            <div className={`row mt-3`}>
-                {renderWords()}
-
-
-            </div>
-
-            {/*<button className={`btn btn-primary mt-2 w-100`}><FontAwesomeIcon className={`me-2`} icon={faFilePdf} />Save PDF</button>*/}
-
+  const renderWords = () => {
+    return words.map((word) => {
+      return (
+        <div className={`col-4`} key={word}>
+          <div
+            className="alert alert-primary"
+            role="alert"
+            onClick={() => appendWord(word)}
+          >
+            {word}
+          </div>
         </div>
-    );
+      );
+    });
+  };
+
+  return (
+    <div className={`container`}>
+      <div className={`d-flex justify-content-center`}>
+        <h1 className={`text-dark mt-5`}>Document editor</h1>
+      </div>
+      <div className={`d-flex justify-content-center mb-5 mt-4`}>
+        <label className={`p-2 fw-bold`}>Document type:</label>
+        <select className={`form-select w-50`}>
+          <option>X</option>
+          <option>Y</option>
+        </select>
+      </div>
+      <ReactQuill value={input} onChange={onChangeInput} bounds={`.app`} />
+      <div className={`row mt-3`}>{renderWords()}</div>
+
+      {/*<button className={`btn btn-primary mt-2 w-100`}><FontAwesomeIcon className={`me-2`} icon={faFilePdf} />Save PDF</button>*/}
+    </div>
+  );
 }
 
 export default App;
